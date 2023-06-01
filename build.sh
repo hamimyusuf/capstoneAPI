dfec087b0e8f23ac27517ceec5ab7580721349c9
hamimyusuf <hamimfusuy@gmail.com>
2023-06-01 20:37:45 +0700

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
+sudo apt update
+sudo apt-get upgrade
 sudo apt-get install git -y
+sudo apt-get install docker-compose -y
 sudo apt-get install docker -y
 sudo apt-get install default-libmysqlclient-dev -y
 source ./capenv/bin/activate
 echo "Install Dependencies"
-sleep 10
+sleep 3
 pip install -r requirements.txt
 echo "Menjalankan Server"
 python3 main.py

f5a7a64f7448362c270f769bfb675aa5a12a25b2
hamimyusuf <hamimfusuy@gmail.com>
2023-06-01 15:11:59 +0700

    revisi deploy

--- coba.py
+++ coba.py
@@ -26,27 +26,6 @@
 #     data.append(temp)
 # print(data)
 
-
-# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
-# import openai
-
-# key = "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"
-
-# openai.api_key = key
-
-# def chat_with_gpt(prompt):
-#     response = openai.Completion.create()
-
-#     if 'choices' in response and len(response['choices']) > 0:
-#         return response['choices'][0]['text']
-#     else:
-#         return None
-
-# prompt = 'Halo, apa kabar?'
-# response = chat_with_gpt(prompt)
-# if response:
-#     print(response)
-
 import requests
 
 url = 'https://story-api.dicoding.dev/v1/register'

ba0650587626c51adf4efca877124c14739d2d4d
hamimyusuf <hamimfusuy@gmail.com>
2023-06-11 05:28:05 +0700

    update

--- functions/connection.py
+++ functions/connection.py
@@ -1,15 +1,36 @@
 import mysql.connector
+from mysql.connector import Error
+from pymongo import MongoClient
+import motor.motor_asyncio
 
 def conn():
-    mydb = mysql.connector.connect(
-    host="localhost",
-    port=3308,
-    user="root",
-    password="123",
-    database="capstone_db"
-    )
-    cursor = mydb.cursor()
-    return {
-        "conn": mydb,
-        "cursor": cursor
-    }
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            port=3308,
+            user="root",
+            password="123",
+            database="capstone_db",
+            autocommit=True
+        )
+        cursor = mydb.cursor()
+        return {
+            "conn": mydb,
+            "cursor": cursor
+        }
+    except Error as e:
+        print(f"Kesalahan koneksi MySQL: {e}")
+        print("Mencoba koneksi ulang...")
+        # Menjalankan kembali fungsi conn() secara rekursif
+        return conn()
+        
+
+def conmongo():
+    client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://root:123@localhost:27017/')
+    db = client.capstone_db
+    dbinfo = db["information"]
+    return dbinfo
+
+
+
+

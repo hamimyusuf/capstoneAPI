0205700726d9af09aec8bf3d2c553aaaf0008d41
hamimyusuf <hamimfusuy@gmail.com>
2023-06-11 10:07:08 +0700

    update

--- controlers/history_controler.py
+++ controlers/history_controler.py
@@ -1,28 +1,39 @@
 from bson.objectid import ObjectId
 from functions.connection import conn
 from functions.generate_id import makeId
+from functions.connection import conmongo
 import mysql.connector
 
 connection = conn()
+data_info = conmongo()
 
-def FormHelper(HistoryData) -> dict:
+def FormHelper(HistoryData,info) -> dict:
     return{
         "ID": str(HistoryData[0]),
-        "date": str(HistoryData[1]),
-        "image": str(HistoryData[2]),
-        "tanaman": str(HistoryData[3]),
-        "penyakit": str(HistoryData[4]),
-        "deskripsi": str(HistoryData[5]),
-        "solusi": str(HistoryData[6])
+        "date": str(HistoryData[2]),
+        "image":str(HistoryData[1]) ,
+        "tanaman":str(info['tanaman']),
+        "penyakit":str(info['penyakit']),
+        "deskripsi": str(info['deskripsi']),
+        "penyebab": str(info['penyebab']),
+        "solusi": info['solusi'],
+        "source": str(info['source']),
+        "penulis": str(info['penulis'])
     }
 
 async def showDataHistory(username: str) -> dict:
-    sql = "SELECT HISTORY.ID_HISTORY,FILENAME,HISTORY.DATE,TANAMAN,PENYAKIT,DESKRIPSI,SOLUSI FROM HISTORY JOIN INPUT ON HISTORY.ID_INPUT = INPUT.ID_INPUT JOIN RESULT ON HISTORY.ID_RESULT = RESULT.ID_RESULT JOIN INFORMATION ON RESULT.ID_INFO = INFORMATION.ID_INFO WHERE HISTORY.USERNAME = %s;"
+    sql = "SELECT HISTORY.ID_HISTORY,FILENAME,HISTORY.DATE, RESULT.ID_INFO FROM HISTORY JOIN INPUT ON HISTORY.ID_INPUT = INPUT.ID_INPUT JOIN RESULT ON HISTORY.ID_RESULT = RESULT.ID_RESULT WHERE HISTORY.USERNAME = %s;"
     connection["cursor"].execute(sql, (username,))
     check = connection["cursor"].fetchall()
     if check:
         data = []
         for temp in check:
-            data.append(FormHelper(temp))
+            id_info = ObjectId(temp[3])
+            print(id_info)
+            dataInfo = await data_info.find_one({"_id":id_info})
+            print(dataInfo)
+            data.append(FormHelper(temp,dataInfo))
+        # id_info = data[0][]
+        print(data)
         return data
     return None
--- controlers/input_image_controler.py
+++ controlers/input_image_controler.py
@@ -1,27 +1,25 @@
-from bson.objectid import ObjectId
 from functions.connection import conn
 from functions.generate_id import makeId
 from functions.connection import conmongo
-import mysql.connector
 import requests
 import datetime
 
 #Variabel Pengaturan Tanggal
 now = datetime.datetime.now() # Data tanggal hari ini
-tgldibuat = now.strftime("%d/%m/%Y")
+tgldibuat = now.strftime("%Y/%m/%d")
 
 data_info = conmongo()
 connection = conn()
 
-def FormHelper(UserData,plant,img_url,id_result) -> dict:
+def FormHelper(UserData,plant,img_url,id_result,result) -> dict:
     return{
-        "id_result": id_result,
-        "file": img_url,
-        "plant": plant,
-        "result": str(UserData['penyakit']),
+        "id_result": str(id_result),
+        "file": str(img_url),
+        "plant": str(plant),
+        "result": str(result),
         "deskripsi": str(UserData['deskripsi']),
         "penyebab": str(UserData['penyebab']),
-        "solusi": str(UserData['solusi']),
+        "solusi": UserData['solusi'],
         "source": str(UserData['source']),
         "penulis": str(UserData['penulis'])
     }
@@ -35,6 +33,9 @@ async def diseasePredict(username, plant, img_url):
         'image':img_url
         }
     result = requests.post(ml_api_url, json = payload)
+    dataInfo = await data_info.find_one({"penyakit":str(result.text),"tanaman":plant})
+    if dataInfo:
+        id_info = dataInfo['_id']
     id_input = makeId()
     id_result = makeId()
     id_history = makeId()
@@ -42,10 +43,9 @@ async def diseasePredict(username, plant, img_url):
     val = (id_input,id_history,img_url)
     connection["cursor"].execute(sql, val)
     connection["conn"].commit()
-    dataInfo = await data_info.find_one({"penyakit":str(result.text),"tanaman":plant})
-    id_info = dataInfo["_id"]
+
     sql = "INSERT INTO RESULT (ID_RESULT, ID_HISTORY, ID_INFO) VALUES (%s, %s, %s)"
-    val = (id_result,id_history,id_info)
+    val = (id_result,id_history,str(id_info))
     connection["cursor"].execute(sql, val)
     connection["conn"].commit()
     sql = "INSERT INTO HISTORY (ID_HISTORY, USERNAME, ID_INPUT, ID_RESULT, DATE) VALUES (%s, %s, %s, %s, %s)"
@@ -53,6 +53,6 @@ async def diseasePredict(username, plant, img_url):
     connection["cursor"].execute(sql, val)
     connection["conn"].commit()
 
-    return FormHelper(data_info, plant, img_url, id_result)
+    return FormHelper(dataInfo, plant, img_url, id_result, result.text)
     
        
